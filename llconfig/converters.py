314040398938539b0de5bd8d567082e83f4cde82
Tomas Bedrich <ja@tbedrich.cz>
2018-07-24 14:19:38 +0200

    Add other falsy options for bool_like, make json import private

--- llconfig/converters.py
+++ llconfig/converters.py
@@ -1,9 +1,9 @@
-from json import loads as json_loads
+import json as _json
 
 
 def bool_like(val):
-    return str(val).lower() not in ('false', '0', 'no', 'off', '')
+    return str(val).lower() not in ('false', '0', 'no', 'off', 'disable', 'disabled', '')
 
 
 def json(val):
-    return json_loads(val, encoding='utf-8')
+    return _json.loads(val, encoding='utf-8')
