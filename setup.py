65105985dab97611bddd67699ba2f1151a6aef07
Tomas Bedrich <ja@tbedrich.cz>
2018-07-25 14:39:20 +0200

    Bump version to 1.0.0, add setup metadata, ignore build dir

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
+from pathlib import Path
+
 from setuptools import setup
 
 setup(
     name='llconfig',
-    version='0.0.1',
+    version='1.0.0',
     packages=['llconfig'],
     license='MIT',
     author='Heureka.cz',
     author_email='vyvoj@heureka.cz',
     description='Lightweight layered configuration library.',
+    long_description=Path(__file__).with_name('README.md').read_text(encoding='utf-8'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/heureka/llconfig',
 )
