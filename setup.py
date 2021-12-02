351fb9e6a4a737232459b5aed33b4b3be307b36a
OndraK <ondrej.kajinek@applifting.cz>
2021-12-02 11:19:28 +0100

    Fix converters.json for python 3.9 and above.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='llconfig',
-    version='2.0.0',
+    version='2.0.1',
     packages=['llconfig'],
     license='Apache 2.0',
     author='Heureka.cz',
